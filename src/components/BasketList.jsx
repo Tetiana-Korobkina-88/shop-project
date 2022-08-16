da3b84213a231f5b28d740fd7f422c521c569ddf
Tetiana-Korobkina-88 <kor1988tanya@gmail.com>
2022-08-16 10:10:38 +0200

    calculated the cost of the order

--- src/components/BasketList.jsx
+++ src/components/BasketList.jsx
@@ -3,6 +3,10 @@ import { BasketItem } from "./BasketItem";
 function BasketList(props) {
   const { order = [], handleBasketShow = Function.prototype } = props;
 
+  const totalPrice = order.reduce((sum, el) => {
+    return sum + el.price.regularPrice * el.quantity;
+  }, 0);
+
   return (
     <ul className="collection basket-list">
       <li className="collection-item active">Basket</li>
@@ -11,7 +15,7 @@ function BasketList(props) {
       ) : (
         <li className="collection-item">empty basket</li>
       )}
-      <li className="collection-item active">Total cost</li>
+      <li className="collection-item active">Total cost: {totalPrice} UAH</li>
       <i class="material-icons basket-close" onClick={handleBasketShow}>
         close
       </i>
